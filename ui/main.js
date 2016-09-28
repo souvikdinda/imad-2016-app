2a376809c8916e1847b30c91f0cbec64363f2d4a
souvikdinda <souvik.dinda.sd@gmail.com>
2016-09-28 19:47:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,41 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+    
+    //Create a request object
+  var request = new XMLHttpRequest();
   
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+         //Take some action
+         if (request.status === 200) { 
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
+         }
+      }
+      //Not done yet
+  };
+
+   //Make the request
+   request.open('GET','http://http://souvikdinda.imad.hasura-app.io/counter',true);
+   request.send(null);
+};
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //Make a request to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+   var list = '';
+   for (var i=0; i<names.length; i++) {
+       list += '<li>' + names[i] + '</li>';
+   }
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
 };
