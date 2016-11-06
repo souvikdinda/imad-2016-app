4ddfe2143f7347f76de0a59c92e8d61b851407e5
souvikdinda <souvik.dinda.sd@gmail.com>
2016-11-06 19:00:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,18 +13,6 @@ app.get('/counter', function (req,res){
    counter=counter+1;
    res.send(counter.toString());
 });
-app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
-});
-
-app.get('/article-three',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
